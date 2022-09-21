0d6ceb02677a0fac43e798bd59561ea0920fe5d0
El Kalinin <112701291+ElKalinin@users.noreply.github.com>
2022-09-21 13:01:18 +0300

    add part 1 of test QA213 (#1)
    * add part 1 of test QA213
    * add changes of past commit
    * add changes of QA213
    * add test commit

---
+++ cypress/e2e/0-jb-tests/QA213.cy.js
@@ -0,0 +1,101 @@
+//Hi, guys!!!
+
+describe('Test QA213',()=>{
+    it('Column 1',()=>{
+        cy.visit('https://qa-gdjsmns.hk/ru/sportsbook/home', {
+            auth: {
+                username: 'joinbet',
+                password: 'ajvxeASUkr'
+            }
+        })
+
+        cy.get('[class="Menu_root__P08M2"]').contains('Ставки')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Линия')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Лайв')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Киберспорт')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Статистика')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Результаты')
+            .should('be.visible')
+    })
+
+    it('Column 2',()=>{
+        cy.visit('https://qa-gdjsmns.hk/ru/sportsbook/home', {
+            auth: {
+                username: 'joinbet',
+                password: 'ajvxeASUkr'
+            }
+        })
+//@TODO убрать строку
+        cy.get('[class="Menu_root__P08M2"]').contains('Казино')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Слоты')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Лайв казино')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Новинки')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Топ игры')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Джекпоты')
+            .should('be.visible')
+    })
+
+    it('Column 3',()=>{
+        cy.visit('https://qa-gdjsmns.hk/ru/sportsbook/home', {
+            auth: {
+                username: 'joinbet',
+                password: 'ajvxeASUkr'
+            }
+        })
+
+        cy.get('[class="Menu_root__P08M2"]').contains('Информация')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('О нас')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Конфиденциальность')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('FAQ')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('AML & KYC')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Ответственная игра')
+            .should('be.visible')
+    })
+
+    it('Column 4',()=>{
+        cy.visit('https://qa-gdjsmns.hk/ru/sportsbook/home', {
+            auth: {
+                username: 'joinbet',
+                password: 'ajvxeASUkr'
+            }
+        })
+
+        cy.get('[class="Menu_root__P08M2"]').contains('Бонусы')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Вступительный бонус')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Условия')
+            .should('be.visible')
+    })
+
+    it('Column 5',()=>{
+        cy.visit('https://qa-gdjsmns.hk/ru/sportsbook/home', {
+            auth: {
+                username: 'joinbet',
+                password: 'ajvxeASUkr'
+            }
+        })
+
+        cy.get('[class="Menu_root__P08M2"]').contains('Полезное')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Прием платежей')
+            .should('be.visible')
+        cy.get('[class="Menu_root__P08M2"]').contains('Политика ставок')
+            .should('be.visible')
+    })
+})
