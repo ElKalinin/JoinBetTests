822e3681ffe43dd3efefb0cca8ba7977de61ebcb
El Kalinin <i.kalinin@joinbet.com>
2022-09-21 13:29:34 +0300

    rewrote the file with any lang rewrote the file delete spaces and rename with lowerCamelCase add new version with export and import rename file rename add changes with new name after review add changes after review add file for vis Andrew and Artem

---
+++ cypress/fixtures/selectors/index.js
@@ -0,0 +1,24 @@
+export const footer = {
+    links: {
+        line: 'a[target="_self"][href*="sportsbook/home"]', //'Линия'
+        live: 'a[target="_self"][href*="sportsbook/live"]', //'Лайв'
+        cybersport: 'a[target="_self"][href*="sportsbook/esports"]', //'Киберспорт'
+        statistics: 'a[target="_blank"][href*="https://stats"]', //'Статистика'
+        results: 'a[target="_blank"][href*="https://livescore"]', //'Результаты'
+        slots: 'a[target="_self"][href*="casino/categories/2254"]', //'Слоты'
+        liveCasino: 'a[target="_self"][href*="casino/live"]', //'Лайв казино'
+        news: 'a[target="_self"][href*="casino/categories/2245"]', //'Новинки'
+        topGames: 'a[target="_self"][href*="casino/categories/2242"]', //'Топ игры'
+        jackpots: 'a[target="_self"][href*="casino/categories/2248"]', //'Джекпоты'
+        about: 'a[target="_self"][href*="info/about"]', //'О нас'
+        privacy: 'a[target="_self"][href*="info/privacy"]', //'Конфиденциальность'
+        faq: 'a[target="_self"][href*="info/faq"]', //'FAQ'
+        amlKyc: 'a[target="_self"][href*="info/aml-kyc"]', //'AML & KYC'
+        responsibleGambling: 'a[target="_self"][href*="info/responsiblegambling"]', //'Ответственная игра'
+        welcomeBonus: 'a[target="_self"][href*="casino/lobby#"]', //'Вступительный бонус'
+        terms: 'a[target="_self"][href*="promo"]', //'Условия'
+        payments: 'a[target="_self"][href*="info/payments"]', //'Прием платежей'
+        bettingPolicy: 'a[target="_self"][href*="info/bettingpolicy"]' //'Политика ставок'
+    },
+    titles: '[class*="Menu_label"]' //'Cтавки','Казино','Информация','Бонусы','Полезное'
+}
