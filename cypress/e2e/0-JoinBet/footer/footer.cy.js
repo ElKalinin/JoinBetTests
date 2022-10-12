51e094df9869938f0a629cbf5c56997dda588e10
El Kalinin <i.kalinin@joinbet.com>
2022-10-12 13:25:28 +0300

    add request code 200 add test for link navigation in footer

--- cypress/e2e/0-JoinBet/footer/footer.cy.js
+++ cypress/e2e/0-JoinBet/footer/footer.cy.js
@@ -1,26 +1,55 @@
 import { footer } from '../../../fixtures/selectors';
 import langs from '../../../fixtures/translations/navigation/footer';
+import urls from '../../../fixtures/url/footer';
 
-langs.forEach(({lang, values})=>{
-
-    describe(`Casino. Footer. ${lang}`,()=> {
-        it(`Отображение всех текстовых данных - язык ${lang}`, () => {
-            cy.visit(`https://st2-gdjsmns.hk/${lang}/casino/lobby`, {
-                auth: {
-                    username: 'joinbet',
-                    password: 'ajvxeASUkr'
-                }
-            })
-            const selectors = Object.values(footer.links)
-            selectors.forEach((selector, index) => {
-                cy.get(selector).should('have.text', Object.values(values)[index])
-            })
-            cy.get(footer.titles)
-                .should('have.length', 5)
-                .each((element, index) => {
-                    cy.get(element).should('have.text', Object.values(values.headLines)[index])
+        describe('Localization. Casino. Footer.', () => {
+            langs.forEach(({lang, values}) => {
+                it(`Отображение всех текстовых данных - язык ${lang}`, () => {
+                    cy.visit(`https://st2-gdjsmns.hk/${lang}/casino/lobby`, {
+                        auth: {
+                            username: 'joinbet',
+                            password: 'ajvxeASUkr'
+                        }
+                    })
+                    const selectors = Object.values(footer.links)
+                    selectors.forEach((selector, index) => {
+                        cy.get(selector).should('have.text', Object.values(values)[index])
+                    })
+                    cy.get(footer.titles)
+                        .should('have.length', 5)
+                        .each((element, index) => {
+                            cy.get(element).should('have.text', Object.values(values.headLines)[index])
+                        })
                 })
+            })
         })
-    })
-})
 
+            describe('Link navigation. Casino. Footer.',()=>{
+                it('Переход по ссылкам', () => {
+                    cy.visit(`https://st2-gdjsmns.hk/ru/casino/lobby`, {
+                        auth: {
+                            username: 'joinbet',
+                            password: 'ajvxeASUkr'
+                        }
+                    })
+                    Object.values(footer.links).forEach((selector, index) => {
+                        cy.scrollTo('bottom')
+                        cy.wait(3000)
+                        cy.get(selector)
+                            .click()
+                        cy.url().should('include', urls[index])
+                        cy.request({
+                            url: urls[index],
+                            auth: {
+                                username: 'joinbet',
+                                password: 'ajvxeASUkr'
+                            },
+                            failOnStatusCode: false
+                        })
+                            .then((response)=>{
+                                expect(response.status).to.eq(200)
+                            })
+                        cy.go('back')
+                    })
+                })
+            })
---
+++ cypress/fixtures/url/footer/index.js
@@ -0,0 +1,21 @@
+export default [
+    '/sportsbook/home',
+    '/sportsbook/live',
+    '/sportsbook/esports',
+    //statisticsUrl: `Статистика`,
+    //resultsUrl: `Результаты`,
+    '/casino/categories/2254',
+    '/casino/live',
+    '/casino/categories/2245',
+    '/casino/categories/2242',
+    '/casino/categories/2248',
+    '/info/about',
+    '/info/privacy',
+    '/info/faq',
+    '/info/aml-kyc',
+    '/info/responsiblegambling',
+    //'https://st2-gdjsmns.hk/ru/casino/lobby#',
+    '/ru/promo',
+    '/info/payments',
+    '/info/bettingpolicy'
+]
