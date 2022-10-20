6406a6a0029173eca1328c2a91f8b900188c904e
El Kalinin <i.kalinin@joinbet.com>
2022-10-20 20:48:20 +0300

    finish version add request code 200 and delete new mistakes

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    stand1: {
+      username: "joinbet",
+      password: "ajvxeASUkr",
+    },
+  },
+
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-  viewportWidth: 1920,
-  viewportHeight: 1080,
 });
